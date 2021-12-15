cff323289c276f1434d55d3e8cc0371989e14cb4
SileboxUnderfined <valerkafm@gmail.com>
2021-12-15 22:39:08 +0300

    procfile update

--- main.py
+++ main.py
@@ -41,4 +41,4 @@ if __name__ in "__main__":
         #userSession = vk_api.VkApi(login=os.environ['USER_PHONE'], password=os.environ['USER_PASSWORD'])
         #userSession.auth()
         #us = userSession.get_api()
-        app.run(port=os.environ['PORT'], debug=False)
+        app.run(debug=False)
