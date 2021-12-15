ef9836ebd8885a0228f087f8ac49d5f9df24775c
SileboxUnderfined <valerkafm@gmail.com>
2021-12-15 22:09:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -41,4 +41,4 @@ if __name__ in "__main__":
         #userSession = vk_api.VkApi(login=os.environ['USER_PHONE'], password=os.environ['USER_PASSWORD'])
         #userSession.auth()
         #us = userSession.get_api()
-        app.run()
+        app.run(port=os.environ['PORT'])
