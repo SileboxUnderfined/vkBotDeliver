f9513fc22850e6635c782616852cc5f3fc8fa475
SileboxUnderfined <valerkafm@gmail.com>
2021-12-15 23:45:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def index():
 @app.route('/myBot', methods=['POST'])
 def bot():
         data = request.get_json(force=True,silent=True)
-        print(data['type'], os.environ['CONFIRMATION_TOKEN'])
         if not data or 'type' not in data:
                 return 'not ok'
 
