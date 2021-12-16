e82b029491daca50a2345fdefaca959979d2e5b7
SileboxUnderfined <valerkafm@gmail.com>
2021-12-16 21:08:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -55,8 +55,7 @@ def captchaHandler(captcha):
 if __name__ in "__main__":
         BotSession = vk_api.VkApi(token=os.environ['VK_API_KEY'])
         bs = BotSession.get_api()
-        userSession = vk_api.VkApi(token=os.environ['SERVICE_KEY'])
-
+        userSession = vk_api.VkApi(token=os.environ['SERVICE_KEY'], app_id=int(os.environ['APP_ID']))
         us = userSession.get_api()
         users = bs.groups.getMembers(group_id=int(os.environ['GROUP_ID']))
         app.run(host="0.0.0.0",port=os.environ['PORT'],debug=False)
